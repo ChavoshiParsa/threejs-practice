3d989250444b6a768aaf80933fbe2e11642d0d34
Your Parsa <parypary82@gmail.com>
2024-04-04 18:36:16 +0330

    done with threejs and add some navbar and title

--- main.js
+++ main.js
@@ -1,2 +1,90 @@
+import gsap from "gsap";
 import "./style.css";
-import * as THREE from "three";
+import * as Three from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// Scene
+const scene = new Three.Scene();
+
+// Create a shape
+const geometry = new Three.SphereGeometry(3, 64, 64);
+const material = new Three.MeshStandardMaterial({
+  color: "#44f",
+});
+const mesh = new Three.Mesh(geometry, material);
+scene.add(mesh);
+
+// Light
+const light = new Three.PointLight(0xffffff, 500);
+light.position.set(10, 10, 10);
+scene.add(light);
+
+// Camera
+const camera = new Three.PerspectiveCamera(45, sizes.width / sizes.height);
+camera.position.z = 20;
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector("#canvas");
+const renderer = new Three.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+renderer.setPixelRatio(2);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
+
+// Handling resize
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+const loop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+loop();
+
+// Timeline
+const timeline = gsap.timeline({ defaults: { duration: 1 } });
+timeline.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
+timeline.fromTo("nav", { y: "-100%" }, { y: "0%" });
+timeline.fromTo(".title", { opacity: 0 }, { opacity: 1 });
+
+// Mouse Animation Color
+let mouseDown = false;
+let rgb = [];
+window.addEventListener("mousedown", () => (mouseDown = true));
+window.addEventListener("mouseup", () => (mouseDown = false));
+
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      200,
+    ];
+  }
+  // animate
+  let newColor = new Three.Color(`rgb(${rgb.join(",")})`);
+  gsap.to(mesh.material.color, { ...newColor });
+});
